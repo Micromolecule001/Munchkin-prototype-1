90ac1f891402f7019b1d888619e6b7562ea52d02
Micromolecule001 <dima.boyko.bd@gmail.com>
2023-12-30 09:06:36 +0200

    add server.js with connection to mongoDB

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import './';
 import React from 'react';
 import Header from './components/header';
 import Users from './components/users';
 import './css/main.css';
 import Cards from './components/cards';
 
-
-
 // main component done as a class
 // App component that shows all other components
 class App extends React.Component {
-
   // html
   render() {
     return (
       <main>
         <Header title="Munchkin" />
       
-        <Users />
 
         <Cards />
       </main>
