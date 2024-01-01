078c82f2c42c5016329eb29813701089adff729e
Micromolecule001 <dima.boyko.bd@gmail.com>
2024-01-01 10:30:11 +0200

    #2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Header from './components/header';
-import Users from './components/users';
 import './css/main.css';
 import Cards from './components/cards';
+
 
 // main component done as a class
 // App component that shows all other components
--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,31 @@
 import React from 'react';
 
 
-/* 
+
 // Header component | default funtction
 function Header(props) {
   return (
     <header>
 
-      <h1 className="title"> {this.props.title} </h1> 
-        
+      {/* Accessing props using this.props */}  
+      <h1 className="title"> {props.title} </h1> 
     </header>
   )
 }
-*/
 
 
+/* 
 // Header component | class
 class Header extends React.Component {
     render() {
       return (   
         <header>
-          {/* Accessing props using this.props */}
+          
           <h1 className="title">{this.props.title}</h1>
           <p> </p>
         </header>
       )
     }
 }
-
+*/
 export default Header;
